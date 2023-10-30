667a62c7265d9b4600b81dd472820794793578f9
Nivans <128242885+NIvanszz@users.noreply.github.com>
2023-10-30 19:45:50 -0300

    Add files via upload

---
+++ autobuilding/1Aautomacao.py
@@ -0,0 +1,22 @@
+import pyautogui as pa
+import mouse as mouse
+
+
+
+pa.press("win")
+pa.sleep(1)
+pa.write("Chrome")
+pa.press("ENTER")
+pa.sleep(3)
+pa.write("youtube.com")
+pa.press("ENTER")
+pa.sleep(4.5)
+pa.click(607,112)
+pa.write("spiderman raindrops keep falling on my head ")
+pa.sleep(2.23)
+pa.press("ENTER")
+pa.sleep(2.5)
+pa.doubleClick(442,296)
+pa.sleep(2)
+pa.press("F")
+
